e798ff9b6371fc6ca583578b10b64582af59bce2
zHrithikcr7 <hrithikgoyal1395@gmail.com>
2023-08-20 19:37:32 +0530

    code added

---
+++ client/build/precache-manifest.7f5c3f5e9f2e365522cbd2f4887e0941.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "0904b836cc382c52cee9",
+    "url": "/static/css/main.034978f5.chunk.css"
+  },
+  {
+    "revision": "0904b836cc382c52cee9",
+    "url": "/static/js/main.fb459192.chunk.js"
+  },
+  {
+    "revision": "d3f399e07026725e9c6e",
+    "url": "/static/js/runtime~main.72b498b8.js"
+  },
+  {
+    "revision": "b945f2fbc3dfc72f1f02",
+    "url": "/static/css/2.57b388f7.chunk.css"
+  },
+  {
+    "revision": "b945f2fbc3dfc72f1f02",
+    "url": "/static/js/2.09a4eb67.chunk.js"
+  },
+  {
+    "revision": "958b89559fe6204d24e2",
+    "url": "/static/js/3.67b868e1.chunk.js"
+  },
+  {
+    "revision": "83ac6bfe23bac3ef412d540b626226ef",
+    "url": "/index.html"
+  }
+];
